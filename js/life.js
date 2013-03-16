858eb0bca81abd6b59840ec7b065138ebdd347ee
keepsleeping <satbirdd@gmail.com>
2013-03-16 23:51:50 +0800

    2 dimon array

--- js/life.js
+++ js/life.js
@@ -4,11 +4,29 @@ function Body(arr) {
 	}
 
 	this.tick =function() {
-		var old_structure = this.structure;
+		this.old_structure = this.structure;
+		this.length = this.old_structure.length;
 		this.structure = [ ];
-		for( var i = 0; i < old_structure.length; i ++) {
-			this.structure.push(0);
+		for(var y = 0; y < this.length; y ++) {
+			this.structure[y] = [];
+			var	x_axe = this.structure[y];
+			for(var x = 0; x < this.old_structure[y].length; x ++) {
+				if (this.live(x, y)) {
+					x_axe.push(1);
+				} else {
+					x_axe.push(0);
+				}
+			}
 		}
+	}
+
+	this.live = function(x_index, y_index) {
+		if (0 == x_index || x_index == this.old_structure[y_index].length) return false;
+		if (1 == this.old_structure[y_index][x_index - 1] 
+			  && 1 == this.old_structure[y_index][x_index +  1]) {
+			return true;
+		}
+		return false;
 	}
 
 	this.initialize(arr);
--- js/test.js
+++ js/test.js
@@ -1,4 +1,4 @@
-module("1 cells")
+module("1 lines, 1 cells")
 function process_cell(given, expected){
 	var body = new Body(given)
 	body.tick();
@@ -6,42 +6,76 @@ function process_cell(given, expected){
 }
 
 test( "1 cell alive will die", function() {
-	process_cell([ 1 ], [0])
+	process_cell([[1]], [[0]])
 });
 
 test("1 cell dead will still be dead", function(){
-	process_cell([0], [0])
+	process_cell([[0]], [[0]])
 });
 
 
 
-module("2 cells")
+module("1 lines, 2 cells")
 test("2 cells alive will all die", function() {
-	process_cell([1, 1], [0, 0])
+	process_cell([[1, 1]], [[0, 0]])
 });
 
 test("2 cells dead will still all die", function() {
-	process_cell([0, 0], [0, 0])
+	process_cell([[0, 0]], [[0, 0]])
 });
 
 test("1 alive 1 dead, both die ", function(){
-	process_cell([1, 0], [0, 0])
+	process_cell([[1, 0]], [[0, 0]])
 }) 
 
-module("3 cells")
+
+
+module("1 lines, 3 cells")
 test("3 dead cell will still be all die", function(){
-	process_cell([0, 0, 0], [0, 0, 0])
+	process_cell([[0, 0, 0]], [[0, 0, 0]])
 })
 
 test("1 alive and 2 dead dead cells result in all die", function(){
-	process_cell([1, 0, 0], [0, 0, 0])
-	process_cell([0, 1, 0], [0, 0, 0])
+	process_cell([[1, 0, 0]], [[0, 0, 0]])
+	process_cell([[0, 1, 0]], [[0, 0, 0]])
 })
 
 test("2 alive and 1 dead cells result 3 dead cell", function(){
-	process_cell([1, 1, 0], [0, 0, 0])
+	process_cell([[1, 1, 0]], [[0, 0, 0]])
 })
 
 test("1 alive and 1 dead and 1 alive cells result in one alive cell", function(){
-	process_cell([1, 0, 1], [0, 1, 0])
+	process_cell([[1, 0, 1]], [[0, 1, 0]])
 })
+
+
+
+module("1 lines, 4 cells")
+test("1 alive cell with 3 dead cell will all die", function(){
+	process_cell([[1, 0, 0, 0]], [[0, 0, 0, 0]]);
+	process_cell([[0, 1, 0, 0]], [[0, 0, 0, 0]]);
+})
+
+test("2 alive cell with 2 dead cells will all die", function(){
+	process_cell([[1, 1, 0, 0]], [[0, 0, 0, 0]]);
+	process_cell([[0, 1, 1, 0]], [[0, 0, 0, 0]]);
+})
+
+test("1 alive with one dead with one alve with one dead will survive one cell", function(){
+	process_cell([[1, 0, 1, 0]], [[0, 1, 0, 0]]);
+	process_cell([[0, 1, 0, 1]], [[0, 0, 1, 0]]);
+})
+
+test("3 alive with 1 dead cell will  survive one cell", function(){
+	process_cell([[1, 1, 1, 0]], [[0, 1, 0, 0]]);
+	process_cell([[1, 1, 0, 1]], [[0, 0, 1, 0]]);
+})
+
+test("4 alive cells wil survive 2 cells", function(){
+	process_cell([[1, 1, 1, 1]], [[0, 1, 1, 0]]);
+})
+
+// module("2 lines, 2 cells")
+// test("2 dead cells will result in 2 dead cells", function(){
+// 	process_cell([[[[0]], [[0]]]], [[[[0]], [[0]]]])
+// })
